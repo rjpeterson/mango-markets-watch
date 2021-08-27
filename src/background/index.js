3331e4ed6949708409ecd9a69cec99e4c567edac
rjpeterson <r.james.peterson@gmail.com>
2021-08-27 14:38:11 +0800

    change from api fetch to mango RPC call

--- src/background/index.js
+++ src/background/index.js
@@ -2,33 +2,62 @@
 // import { Connection, PublicKey } from `@solana/web3.js`;
 const mango_client = require('@blockworks-foundation/mango-client');
 const web3 = require('@solana/web3.js');
-const { IDS, MangoClient } = mango_client;
+const { IDS, MangoClient, Config, I80F48 } = mango_client;
 const { Connection, PublicKey } = web3;
-// console.log(`IDS: ${JSON.stringify(IDS)}`)
 
-const getRates = async () => {
+const getTokenInfo = async () => {
   const cluster = 'mainnet';
-  const group = 'mainnet.1';
+  const groupName = 'mainnet.1';
+  const config = new Config(IDS);
   const clusterIds = IDS.groups[0]; //TODO need to find "group" in IDS array
+  // console.log(`clusterIds token info: ${JSON.stringify(clusterIds.tokens)}`)
 
+  const groupConfig = config.getGroup(cluster, groupName);
   const connection = new Connection(IDS.cluster_urls[cluster], 'singleGossip');
-  const mangoGroupPk = new PublicKey(clusterIds.mangoProgramId);
-  const client = new MangoClient();
+  const mangoProgramId = new PublicKey(clusterIds.mangoProgramId);
+  const mangoGroupKey = groupConfig.publicKey;
+  const client = new MangoClient(connection, mangoProgramId);
   // const srmVaultPk = new PublicKey(clusterIds.serumProgramId);
   // const mangoGroup = await client.getMangoGroup(connection, mangoGroupPk, srmVaultPk);
-  const mangoGroup = await client.getMangoGroup(mangoGroupPk);
-  const rootBanks = mangoGroup.loadRootBanks(connection);
-  let depositRates = []
-  let borrowRates = []
-  rootBanks.forEach(rootBank => {
-    const borrowRate = rootBank.getBorrowRate(mangoGroup)
-    const depositRate = rootBank.getDepositRate(mangoGroup)
-    depositRates.push(depositRate)
-    borrowRates.push(borrowRate)
-  })
-  console.log(`depositRates: ${JSON.stringify(depositRates)}`)
-  console.log(`borrowRates: ${JSON.stringify(borrowRates)}`)
+
+  const mangoGroup = await client.getMangoGroup(mangoGroupKey);
+  if (mangoGroup) {
+    const rootBanks = await mangoGroup.loadRootBanks(connection)
+    const latestStats = groupConfig.tokens.map((token) => {
+      const rootBank = rootBanks.find((bank) => {
+        if (!bank) {
+          return false
+        }
+        return bank.publicKey.toBase58() == token.rootKey.toBase58()
+      })
+      // const totalDeposits = rootBank.getUiTotalDeposit(mangoGroup)
+      // const totalBorrows = rootBank.getUiTotalBorrow(mangoGroup)
+
+      return {
+        // time: new Date(),
+        name: token.symbol,
+        // totalDeposits: totalDeposits.toFixed(
+        //   tokenPrecision[token.symbol] || 2
+        // ),
+        // totalBorrows: totalBorrows.toFixed(
+        //   tokenPrecision[token.symbol] || 2
+        // ),
+        depositRate: rootBank
+          .getDepositRate(mangoGroup)
+          .mul(I80F48.fromNumber(100)).toFixed(2),
+        borrowRate: rootBank
+          .getBorrowRate(mangoGroup)
+          .mul(I80F48.fromNumber(100)).toFixed(2),
+        // utilization: totalDeposits.gt(I80F48.fromNumber(0))
+        //   ? totalBorrows.div(totalDeposits)
+        //   : I80F48.fromNumber(0),
+      }
+    })
+    console.log(`tokenInfo: ${JSON.stringify(latestStats)}`)
+    return latestStats
+  }
 }
+
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log('onInstalled...');
@@ -77,35 +106,34 @@ function scheduleWatchdog() {
 
 //fetch data and save to local storage
 async function startRequest() {
-  let tokens = [];
-  let tokensInfo = [];
-  console.log('start HTTP Request...')
-  const response = await fetch('https://mango-stats-v3.herokuapp.com/spot?mangoGroup=mainnet.1')
-  if (!response.ok) {
-      alert(`Somthing went wrong: ${response.status} - ${response.statusText}`)
-  }
-  rawData = await response.json()
-  let trimmedData = [];
-  for (i = rawData.length; i > 0; i--) {
-    if (!tokens.includes(rawData[i - 1].name)) {
-      tokens.push(rawData[i - 1].name)
-      trimmedData.push(rawData[i - 1])
-    }
-  }
-  trimmedData.map(token => {
-      const trimmedName = token.name.replace('/USDC','')
-      tokensInfo.push({
-          name: trimmedName,
-          borrowRate: (token.borrowRate * 100).toFixed(2),
-          depositRate: (token.depositRate * 100).toFixed(2),
-      })
-  })
+  const tokensInfo = await getTokenInfo()
+  // let tokens = [];
+  // let tokensInfo = [];
+  // console.log('start HTTP Request...')
+  // const response = await fetch('https://mango-stats-v3.herokuapp.com/spot?mangoGroup=mainnet.1')
+  // if (!response.ok) {
+  //     alert(`Somthing went wrong: ${response.status} - ${response.statusText}`)
+  // }
+  // rawData = await response.json()
+  // let trimmedData = [];
+  // for (i = rawData.length; i > 0; i--) {
+  //   if (!tokens.includes(rawData[i - 1].name)) {
+  //     tokens.push(rawData[i - 1].name)
+  //     trimmedData.push(rawData[i - 1])
+  //   }
+  // }
+  // trimmedData.map(token => {
+  //     const trimmedName = token.name.replace('/USDC','')
+  //     tokensInfo.push({
+  //         name: trimmedName,
+  //         borrowRate: (token.borrowRate * 100).toFixed(2),
+  //         depositRate: (token.depositRate * 100).toFixed(2),
+  //     })
+  // })
   chrome.storage.local.set(
     {tokensInfo: tokensInfo},
       function() {
-      console.log(`stored tokensInfo : ${JSON.stringify(tokensInfo)}`)
+      // console.log(`stored tokensInfo : ${JSON.stringify(tokensInfo)}`)
     }
   )
 }
-
-getRates();
