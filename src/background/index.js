f6894a2b283d5653ccc7ab879927652a1151e2af
rjpeterson <r.james.peterson@gmail.com>
2021-08-30 16:04:41 +0800

    fix some v2 errors

--- src/background/index.js
+++ src/background/index.js
@@ -1,24 +1,84 @@
 // import { IDS, MangoGroup } from '@blockworks-foundation/mango-client';
 // import { Connection, PublicKey } from `@solana/web3.js`;
-const mango_client = require('@blockworks-foundation/mango-client');
-const web3 = require('@solana/web3.js');
-const { IDS, MangoClient, Config, I80F48 } = mango_client;
-const { Connection, PublicKey } = web3;
+// const mango_client_v3 = require('@blockworks-foundation/mango-client-v3');
+import { IDS as IDS_v3, MangoClient as MangoClient_v3, Config as Config_v3, I80F48 } from '@blockworks-foundation/mango-client-v3';
+import { IDS, MangoGroup, MangoClient } from '@blockworks-foundation/mango-client';
+import { Connection, PublicKey } from '@solana/web3.js';
 
-const cluster = 'mainnet';
-const groupName = 'mainnet.1';
+const tokenInfoSwitch = async (version) => {
+  const actions = {
+    '1' : function() {
+      console.log('getting tokenInfo version 1 ');
+      return getTokenInfo_v1v2(1)
+    },
+    '2' : function() {
+      console.log('getting tokenInfo version 2 ');
+      return getTokenInfo_v1v2(2)
+    },
+    '3' : function() {
+      console.log('getting tokenInfo version 3 ');
+      return getTokenInfo_v3()
+    },
+  }
+  return await (actions[version]() || actions[3]())
+}
 
-const getTokenInfo = async () => {
-  const config = new Config(IDS);
-  const clusterId = IDS.groups.find(group => {return group.name == groupName && group.cluster == cluster});
+const getTokenInfo_v1v2 = async (version) => {
+  console.log(`getting v${version} token info...`)
+  let cluster = ''
+  let group = ''
+  if (version === 1) {
+    cluster = 'mainnet-beta', group = 'BTC_ETH_USDT'
+  } else {
+    cluster = 'mainnet-beta', group = 'BTC_ETH_SOL_SRM_USDC'
+  }
+  const connection = new Connection(IDS.cluster_urls[cluster], 'singleGossip');
+
+  const mangoGroupPk = new PublicKey(IDS[cluster].mango_groups[group].mango_group_pk);
+  const srmVaultPk = new PublicKey(IDS[cluster].mango_groups[group].srm_vault_pk)
+  const client = new MangoClient();
+  const mangoGroup = await client.getMangoGroup(connection, mangoGroupPk, srmVaultPk);
+
+  if (mangoGroup) {
+    const symbols = IDS[cluster].mango_groups[group].symbols
+    console.log(`symbols: ${JSON.stringify(symbols)}`)
+    const latestStats = Object.keys(symbols).map((tokenSymbol) => {
+      if(!tokenSymbol) {
+        return false
+      } else {
+        console.log(`tokenSymbol: ${JSON.stringify(tokenSymbol)}`)
+        const tokenIndex = mangoGroup.getTokenIndex(new PublicKey(symbols[tokenSymbol]))
+        console.log(`tokenIndex: ${JSON.stringify(tokenIndex)}`)
+        const depositRate = mangoGroup.getDepositRate(tokenIndex)
+        console.log(`depositRate: ${JSON.stringify(depositRate)}`)
+        const borrowRate = mangoGroup.getBorrowRate(tokenIndex)
+        console.log(`borrowRate: ${JSON.stringify(borrowRate)}`)
+        return {
+          name: tokenSymbol,
+          depositRate: depositRate,
+          borrowRate: borrowRate
+        }
+        debugger
+    }})
+    console.log(`v${version} latestStats: ${JSON.stringify(latestStats)}`)
+    return latestStats
+  } else {
+    console.log(`Mango Group not found`)
+  }
+}
+
+const getTokenInfo_v3 = async () => {
+  console.log(`getting v3 token info...`)
+  const cluster = 'mainnet';
+  const groupName = 'mainnet.1';
+  const config = new Config_v3(IDS_v3);
+  const clusterId = IDS_v3.groups.find(group => {return group.name == groupName && group.cluster == cluster});
 
   const groupConfig = config.getGroup(cluster, groupName);
-  const connection = new Connection(IDS.cluster_urls[cluster], 'singleGossip');
+  const connection = new Connection(IDS_v3.cluster_urls[cluster], 'singleGossip');
   const mangoProgramId = new PublicKey(clusterId.mangoProgramId);
   const mangoGroupKey = groupConfig.publicKey;
-  const client = new MangoClient(connection, mangoProgramId);
-  // const srmVaultPk = new PublicKey(clusterIds.serumProgramId);
-  // const mangoGroup = await client.getMangoGroup(connection, mangoGroupPk, srmVaultPk);
+  const client = new MangoClient_v3(connection, mangoProgramId);
 
   const mangoGroup = await client.getMangoGroup(mangoGroupKey);
   if (mangoGroup) {
@@ -53,30 +113,36 @@ const getTokenInfo = async () => {
         //   : I80F48.fromNumber(0),
       }
     })
-    console.log(`tokenInfo: ${JSON.stringify(latestStats)}`)
+    console.log(`v3 tokenInfo: ${JSON.stringify(latestStats)}`)
     return latestStats
+  } else {
+    console.log(`Mango Group not found`)
   }
 }
 
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log('onInstalled...');
+  console.log('scheduling request...');
   scheduleRequest();
+  console.log('scheduling watchdog...');
   scheduleWatchdog();
-  startRequest();
+  console.log('getting initial token info...');
+  startRequest(3);
 });
 
 // fetch and save data when chrome restarted, alarm will continue running when chrome is restarted
 chrome.runtime.onStartup.addListener(() => {
   console.log('onStartup....');
-  startRequest();
+  console.log('getting version and token info...');
+  getVersionStartRequest()
 })
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
     console.log(
       `Storage key "${key}" in namespace "${namespace}" changed.`,
-      `Old value was "${JSON.stringify(oldValue)}", new value is "${JSON.stringify(newValue)}".`
+      `Old value: ${JSON.stringify(oldValue)}, new value: ${JSON.stringify(newValue)}.`
     );
     if (key == "tokensInfo") {
       chrome.runtime.sendMessage({
@@ -85,6 +151,10 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
             content: newValue
         }
       });
+    } else if (key == "version") { 
+      const tokensInfo = tokenInfoSwitch(newValue)
+      console.log(`version changed to ${newValue}. New tokenInfo: ${JSON.stringify(tokensInfo)}`)
+      chrome.storage.local.set({tokensInfo: tokensInfo})
     }
   }
 })
@@ -99,15 +169,28 @@ chrome.alarms.onAlarm.addListener(alarm => {
       } else {
         //if it is no there, start a new request and reschedule refresh alarm
         console.log("Refresh alarm doesn't exist, starting a new one");
-        startRequest();
+        getVersionStartRequest()
         scheduleRequest();
       }
     });
   } else {
     //if refresh alarm triggered, start a new request
-    startRequest();
+    console.log("Refresh alarm triggered");
+    getVersionStartRequest()
   }
 })
+
+function getVersionStartRequest() {
+  chrome.storage.local.get(['version'], result => {
+    if (result) {
+      console.log(`got version from storage. starting request for tokenInfo version: ${result.version}`)
+      startRequest(result.version)
+    } else {
+      console.log(`No version # in storage. starting request for tokenInfo version: 3`)
+      startRequest(3)
+    }
+  })
+}
 
 //schedule a new fetch every 30 minutes
 function scheduleRequest() {
@@ -122,7 +205,12 @@ function scheduleWatchdog() {
 }
 
 //fetch data and save to local storage
-async function startRequest() {
-  const tokensInfo = await getTokenInfo()
-  chrome.storage.local.set({tokensInfo: tokensInfo})
+async function startRequest(version) {
+  console.log('getting token info...')
+  const tokensInfo =  await tokenInfoSwitch(version)
+  console.log(`got version ${version} tokensInfo: ${JSON.stringify(tokensInfo)}`)
+    // const tokensInfo = await getTokenInfo_v3()
+  chrome.storage.local.set({tokensInfo: tokensInfo}, () => {
+    console.log(`sending to chrome storage tokensInfo:${JSON.stringify(tokensInfo)}`)
+  })
 }
