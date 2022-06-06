ddec8b2066251c92464da13d7f52629fc95302b8
rjpeterson <r.james.peterson@gmail.com>
2022-06-06 15:33:30 -0700

    finish connection.test

--- src/background/connection.test.ts
+++ src/background/connection.test.ts
@@ -1,33 +1,75 @@
-
-import { IDS, MangoClient, Config, MangoGroup } from "@blockworks-foundation/mango-client-v3";
+import { IDS, MangoClient, Config } from "@blockworks-foundation/mango-client-v3";
 import { Connection, PublicKey } from "@solana/web3.js";
 import { establishConnection } from "./connection";
 
-jest.mock("@blockworks-foundation/mango-client-v3");
+const mockGroupConfig = {
+  name: "test group",
+  cluster: "devnet",
+  publicKey: "BKvqsuNcnwWqPzzuhLmGi4rzzh55FhJtGizkhHaEJqiV"
+}
+const mockClusterData = {
+  cluster: "devnet",
+  name: "test group",
+  publicKey: "BKvqsuNcnwWqPzzuhLmGi4rzzh55FhJtGizkhHaEJqiV",
+  quoteSymbol: "SOL",
+  mangoProgramId: "AKvqsuNcnwWqPzzuhLmGi4rzzh55FhJtGizkhHaEJqiV",
+  serumProgramId: "AKvqsuNcnwWqPzzuhLmGi4rzzh55FhJtGizkhHaEJqiV",
+}
+const mockIDS = {
+  groups: [mockClusterData]
+}
+const mockConfig = {
+  getGroup: jest.fn().mockImplementation(() => {
+    return mockGroupConfig
+  })
+}
+const mockCache = {
+  cache: "mockCache"
+}
+const mockMangoGroup = {
+  loadCache: () => {
+    return mockCache
+  }
+}
+const mockMangoClient = {
+  getMangoGroup: () => {
+    return mockMangoGroup;
+  }
+}
+jest.mock("@blockworks-foundation/mango-client-v3", () => ({
+  __esModule: true,
+  IDS: {groups: [{
+    cluster: "devnet",
+    name: "test group",
+    publicKey: "BKvqsuNcnwWqPzzuhLmGi4rzzh55FhJtGizkhHaEJqiV",
+    quoteSymbol: "SOL",
+    mangoProgramId: "AKvqsuNcnwWqPzzuhLmGi4rzzh55FhJtGizkhHaEJqiV",
+    serumProgramId: "AKvqsuNcnwWqPzzuhLmGi4rzzh55FhJtGizkhHaEJqiV",
+  }]},
+  Config: jest.fn().mockImplementation(() => { 
+    return mockConfig
+  }),
+  Connection: jest.fn().mockImplementation(() => {}),
+  MangoClient: jest.fn().mockImplementation(() => {
+    return mockMangoClient
+  }),
+}));
 jest.mock("@solana/web3.js")
 
 describe('connection', () => {
+  
   describe('establishConnection', () => {
 
-    // beforeAll(() => {
-    //   MangoClient.mockImplementation(() => {
-    //     return {
-    //       getMangoGroup: jest.fn().mockImplementation(() => {})
-    //     }
-    // })
-
     it('should return the requested objects', async () => {
-      const result = establishConnection();
-      // const configMockInit = Config.mock.instances[0].init;
-      // const connectionMockInit = Connection.mock.instances[0].init;
-      // const clientMockInit = MangoClient.mock.instances[0].init;
-      await new Promise(process.nextTick);
-      expect(PublicKey).toHaveBeenCalled();
-      expect(Config).toHaveBeenCalled();
-      expect(MangoClient).toHaveBeenCalled();
-      expect(MangoClient.prototype.getMangoGroup).toHaveBeenCalled();
-      expect(MangoGroup.prototype.loadCache).toHaveBeenCalled();
-      expect(result).toBeTruthy();
+      const result = await establishConnection("devnet", "test group");
+      expect(result).toStrictEqual({
+        mangoGroup: mockMangoGroup,
+        client: mockMangoClient,
+        connection: expect.any(Connection),
+        groupConfig: mockGroupConfig,
+        clusterData: mockClusterData,
+        mangoCache: mockCache
+      });
     })
   })
 })
